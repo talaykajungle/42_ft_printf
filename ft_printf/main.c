a1b1a9856d0d5f06cce501376bc77d3672b7edd1
Talha <mutlutalha02@gmail.com>
2024-12-29 17:53:25 +0100

    finished ft_printf

---
+++ ft_printf/main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tamutlu <tamutlu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/22 15:53:38 by tamutlu           #+#    #+#             */
+/*   Updated: 2024/12/26 19:51:19 by tamutlu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "ft_printf.h"
+
+// int	main(void)
+// {
+// 	int		number;
+// 	char	character;
+// 	char	*string;
+
+// 	number = 42;
+// 	character = 'A';
+// 	string = "Hello, World!";
+// 	ft_printf("Character: %c\n", character);
+// 	ft_printf("String: %s\n", string);
+// 	ft_printf("Number: %d\n", number);
+// 	ft_printf("Pointer: %p\n", (void *)&number);
+// 	ft_printf("Hexadecimal (lowercase): %x\n", number);
+// 	ft_printf("Hexadecimal (uppercase): %X\n", number);
+// 	ft_printf("Unsigned Number: %u\n", (unsigned int)number);
+// 	ft_printf("Percent sign: %%\n");
+// 	return (0);
+// }
+
+// To run the main you also need to call evry .c file that is connected
+// that includes also the libft library
+
+// The command to run it:
+// cc *.c libft/*.c && ./a.out
